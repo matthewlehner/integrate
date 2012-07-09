97da8939ef69969c6fe755d90e533cfed63eebd8
Matthew Lehner <matthewlehner@gmail.com>
2012-07-09 15:28:30 -0700

    static pages controller specs

---
+++ spec/controllers/static_pages_controller_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe StaticPagesController do
+  describe 'GET home' do
+    it 'renders the home template' do
+      get :home
+      response.should render_template('home')
+    end
+  end
+
+  describe 'GET about' do
+    it 'renders the about template' do
+      get :home
+      response.should render_template('about')
+    end
+  end
+end
