fcc6c4da296519671fa28d998826db2d0bef764f
Matthew Lehner <matthewlehner@gmail.com>
2012-07-09 15:29:27 -0700

    about template and specs

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -10,7 +10,7 @@ describe StaticPagesController do
 
   describe 'GET about' do
     it 'renders the about template' do
-      get :home
+      get :about
       response.should render_template('about')
     end
   end
