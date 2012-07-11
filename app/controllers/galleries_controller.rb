33bd31a6098a0f1e5c3d135469514cb4f7274c98
Matthew Lehner <matthewlehner@gmail.com>
2012-07-11 12:13:34 -0700

    gallery controller specs

--- app/controllers/galleries_controller.rb
+++ app/controllers/galleries_controller.rb
@@ -1,7 +1,9 @@
 class GalleriesController < ApplicationController
   def index
+    @galleries = Gallery.scoped
   end
 
   def show
+    @gallery = Gallery.find params[:id]
   end
 end
--- spec/controllers/galleries_controller_spec.rb
+++ spec/controllers/galleries_controller_spec.rb
@@ -1,16 +1,28 @@
 require 'spec_helper'
 
 describe GalleriesController do
+  let(:gallery) { FactoryGirl.create(:gallery) }
+
   describe "GET #index" do
+    it 'populates an array of galleries' do
+      get :index
+      assigns(:galleries).should eq([gallery])
+    end
+
     it "renders the :index view" do
       get :index
       response.should render_template :index
     end
   end
 
-  describe "GET 'show'" do
-    it "returns http success" do
-      get :show, id: '1'
+  describe "GET #show" do
+    it 'assigns the requested gallery to @gallery' do
+      get :show, id: gallery
+      assigns(:gallery).should eq(gallery)
+    end
+
+    it "renders the :show view" do
+      get :show, id: gallery
       response.should render_template :show
     end
   end
