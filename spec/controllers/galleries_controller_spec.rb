ac7169d8c553bbd78cc0447c4f61a32876b11466
Matthew Lehner <matthewlehner@gmail.com>
2012-07-11 11:38:33 -0700

    passing galleries controller specs

--- spec/controllers/galleries_controller_spec.rb
+++ spec/controllers/galleries_controller_spec.rb
@@ -1,19 +1,17 @@
 require 'spec_helper'
 
 describe GalleriesController do
-
-  describe "GET 'index'" do
-    it "returns http success" do
-      get 'index'
-      response.should be_success
+  describe "GET #index" do
+    it "renders the :index view" do
+      get :index
+      response.should render_template :index
     end
   end
 
   describe "GET 'show'" do
     it "returns http success" do
-      get 'show'
-      response.should be_success
+      get :show, id: '1'
+      response.should render_template :show
     end
   end
-
 end
