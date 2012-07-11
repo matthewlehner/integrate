fb7937ffbea81d106df73e39749c4a4aacdf923c
Matthew Lehner <matthewlehner@gmail.com>
2012-07-11 12:39:35 -0700

    load galleries from csv.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'CSV'
+
+CSV.foreach ::Rails.root + 'lib/galleries.csv', headers: true do |g|
+  puts g[1]
+  gallery = Gallery.new
+  gallery.name = g[1]
+  gallery.address = g[2]
+  gallery.hours = g[3]
+  gallery.festival_hours = g[4]
+  gallery.phone = g[5]
+  gallery.email = g[6]
+  gallery.website = g[7]
+  gallery.description = g[8]
+  gallery.exhibition_description = g[9]
+  gallery.save
+end
