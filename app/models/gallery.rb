6fd88c86b347bdefe61d53ac0affb12d4ea90b5f
Matthew Lehner <matthewlehner@gmail.com>
2012-07-12 15:01:01 -0700

    gallery can edit nested location

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,7 +1,8 @@
 class Gallery < ActiveRecord::Base
   has_one :location, as: :addressable
+  accepts_nested_attributes_for :location
 
-  attr_accessible :name, :phone, :email, :website, :hours, :festival_hours, :description, :exhibition_description, :photo
+  attr_accessible :name, :phone, :email, :website, :hours, :festival_hours, :description, :exhibition_description, :photo, :location_attributes
 
   image_accessor :photo
 
