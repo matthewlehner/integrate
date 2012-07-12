3bfcf7f8eea4e2592258de435fac4ae0623bd066
Matthew Lehner <matthewlehner@gmail.com>
2012-07-12 08:35:32 -0700

    galleries sorted by name

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -2,4 +2,6 @@ class Gallery < ActiveRecord::Base
   attr_accessible :name, :address, :phone, :email, :website, :hours, :festival_hours, :description, :exhibition_description, :photo
 
   image_accessor :photo
+
+  default_scope order("LOWER(name)")
 end
