c2d7d542faa51d120936a4b891b03bbb4d128927
Matthew Lehner <matthewlehner@gmail.com>
2012-07-11 17:48:53 -0700

    dragonfly accessor method.w

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,3 +1,5 @@
 class Gallery < ActiveRecord::Base
   attr_accessible :name, :address, :phone, :email, :website, :hours, :festival_hours, :description, :exhibition_description
+
+  image_accessor :photo
 end
