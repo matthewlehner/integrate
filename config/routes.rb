ccdb3cd04001e767cbf7ff03b7eabb97f8c04445
Matthew Lehner <matthewlehner@gmail.com>
2012-07-11 19:26:24 -0700

    admin/galleries routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Integrate::Application.routes.draw do
+
   resources :galleries, only: [:index, :show]
+
+  namespace :admin do
+    resources :galleries, except: :show
+  end
 
   get 'about', to: 'static_pages#about', as: 'about'
 
