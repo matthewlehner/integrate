429cb460faf104a0e7db8252ed723a473fc867cb
Matthew Lehner <matthewlehner@gmail.com>
2012-07-13 17:34:37 -0700

    locations don't exist without their gallery

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,5 +1,5 @@
 class Gallery < ActiveRecord::Base
-  has_one :location, as: :addressable
+  has_one :location, as: :addressable, dependent: :destroy
   accepts_nested_attributes_for :location
 
   attr_accessible :name, :phone, :email, :website, :hours, :festival_hours, :description, :exhibition_description, :photo, :location_attributes
