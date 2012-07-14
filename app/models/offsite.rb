0cfda8b242f67547352476d84109b025cc0aaac5
Matthew Lehner <matthewlehner@gmail.com>
2012-07-13 17:35:46 -0700

    same with offsite events

--- app/models/offsite.rb
+++ app/models/offsite.rb
@@ -1,5 +1,5 @@
 class Offsite < ActiveRecord::Base
-  has_one :location, as: :addressable
+  has_one :location, as: :addressable, dependent: :destroy
   accepts_nested_attributes_for :location
 
   attr_accessible :details, :name, :schedule, :location_attributes
