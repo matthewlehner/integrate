274e63b8cb02912f13f97f5fc0bcb9d233ca13bd
Matthew Lehner <matthewlehner@gmail.com>
2012-07-19 08:09:39 -0700

    specs for gallery photo

--- spec/models/gallery_spec.rb
+++ spec/models/gallery_spec.rb
@@ -4,11 +4,19 @@ describe Gallery do
   subject { gallery }
   let(:gallery) { FactoryGirl.create(:gallery) }
 
+  it 'has a valid factory' do
+    gallery.should be_valid
+  end
+
   context 'has a picture' do
     it { should respond_to(:photo) }
   end
 
   it 'checks for name' do
     Gallery.new.should have(1).error_on(:name)
+  end
+
+  it 'checks for name' do
+    Gallery.new.should have(1).error_on(:photo)
   end
 end
