e631605b8da3e638e08319c00792cbe32b42ac63
Matthew Lehner <matthewlehner@gmail.com>
2012-07-19 08:25:58 -0700

    ran migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120717233710) do
+ActiveRecord::Schema.define(:version => 20120719151825) do
 
   create_table "galleries", :force => true do |t|
     t.string   "name"
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(:version => 20120717233710) do
     t.string   "logo_uid"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+    t.integer  "position"
   end
 
 end
