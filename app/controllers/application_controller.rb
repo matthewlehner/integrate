eee8122e2f1d6bdc2be6847b00e90d19ad32c158
Matthew Lehner <matthewlehner@gmail.com>
2012-07-23 10:54:17 -0700

    methods to check for mobile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,22 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :check_for_mobile
+
+  def check_for_mobile
+    session[:mobile_override] = params[:mobile] if params[:mobile]
+    prepare_for_mobile if mobile_device?
+  end
+
+  def prepare_for_mobile
+    prepend_view_path Rails.root + 'app/views/mobile_views'
+  end
+
+  def mobile_device?
+    if session[:mobile_override]
+      session[:mobile_override] == '1'
+    else
+      (request.user_agent =~ /Mobile|webOS/) && (request.user_agent !~ /iPad/)
+    end
+  end
+
 end
