09b126cc630c1aa4425ecc474c8e0e016c4e0023
Matthew Lehner <matthewlehner@gmail.com>
2012-08-10 15:15:42 -0700

    just show galleries on the map

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,5 +1,5 @@
 class LocationsController < ApplicationController
   def index
-    @locations = Location.includes(:addressable).where("latitude is not NULL")
+    @locations = Location.includes(:addressable).where("latitude is not NULL").where(addressable_type: 'Gallery')
   end
 end
